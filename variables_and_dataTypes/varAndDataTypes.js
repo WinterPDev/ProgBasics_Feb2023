e2a925af7eb9f62d16265d48ce229bcba3cdc3ca
WinterP <winterp.dev@gmail.com>
2023-02-06 18:55:41 -0600

    Initial Upload

---
+++ variables_and_dataTypes/varAndDataTypes.js
@@ -0,0 +1,29 @@
+// variables are information/data that we can name and call back to!
+
+var favAnimal = "Cheetah";  // Text/String Data Type
+
+console.log(favAnimal); // Print values to our Console/Terminal Output: "Cheetah"
+
+var favAnimal2 = "Calico Cat"; // Assigning a new value to an existing variable
+
+console.log(favAnimal); // Output: 'Calico Cat'
+
+var numPets = 4;      // Number Data Type
+
+var favAnimalName = 4;
+
+console.log('My pets name is: ' + favAnimalName); // My pets name is: 4 
+
+console.log(numPets + 2); // Output: 6 (adding numPets(4) + 2)) 
+
+numPets = numPets + 2;
+
+console.log(numPets); // Output: 6 
+
+console.log(favAnimal + " " + numPets); // Output: cheetah4 
+// If we add a string to a number, that number gets concatenated to the string making them 1 string. 
+
+
+var havePets = true; // Booleans are true/false values
+
+console.log(havePets); // Output: true
