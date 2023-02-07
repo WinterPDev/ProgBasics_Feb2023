6bbcf1c711d996aab8adab1d465ab7585708cec3
WinterP <winterp.dev@gmail.com>
2023-02-07 17:20:45 -0600

    Upload Comments and Conditionals JS

---
+++ CommentsAndConditionals/CommendsAndConditionals.js
@@ -0,0 +1,48 @@
+
+
+// Some words
+// Assign variable pet to pet's name
+var pet = 'Theo';
+
+/*
+
+Build a for loop to go through vacation destinations.
+
+Create conditional for the cheapest vacation idea! 
+
+Check for time of year, wouldn't want to go to Hawaii during tsunami season!
+
+*/
+
+var age = 18;
+
+var location = 'Pheonix, AZ';
+
+// inside of the if statement, you give it a yes or no (true or false) question
+
+if (age >= 18) {
+    // if the condition is met and returns 'true' then it runs the code in the curly braces!
+    console.log('Traveler is older than 18!');
+} else {
+    // else if the condition does not return true, run this code.
+    console.log('Traveler is not older than 18!');
+}
+
+
+
+if (location == 'Aruba') {
+
+    // If the location they are going to is Aruba...do this.
+    console.log("You're going to Aruba, don't pack a jacket!");
+
+} else if (location == 'Pheonix, AZ') {
+
+    // if the location they are going to is not Aruba AND it's Pheonix...do this.
+    console.log("Going to Pheonix, it's gonna be hot there!");
+
+} else {
+
+    // if the location is not Aruba OR Pheonix...do this.
+    console.log('Not going to Pheonix or Aruba!');
+}
+
