64fa36dffdf6a7b187d6e2c7d6031e79b82f2db9
WinterP <winterp.dev@gmail.com>
2023-02-07 12:10:09 -0600

    Added Array/Order Notes

---
+++ OrderAndArrays/OrderAndArrays.js
@@ -0,0 +1,40 @@
+
+
+/*
+
+P - Parenthesis
+E - Exponents
+M - Multiplication
+D - Division
+A - Addition
+S - Subtraction
+
+*/
+
+// Math.pow(5, 2)
+
+
+var dreamVacation = 'Hawaii';
+
+dreamVacation = 'Aruba';
+
+var dreamVacations = ['Hawaii', 'Osaka JP', 'Iceland', 'Bali'];
+// console.log(dreamVacations)
+
+dreamVacations[0] = dreamVacation;
+
+// Name of the Array (The Target we want to change).pushMethod(What we want to add)
+dreamVacations.push('Bristol, UK');
+console.log(dreamVacations);
+
+// dreamVacations.pop();
+
+console.log(dreamVacations.length);
+
+
+// Logging a value from our Array by it's Index!
+// console.log(dreamVacations);
+
+var vacationPlan = ['Hawaii', 4000, false];
+
+console.log(vacationPlan[1] + '12')
